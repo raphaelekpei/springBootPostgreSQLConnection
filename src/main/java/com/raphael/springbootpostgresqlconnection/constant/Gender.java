c5c470c8cf5c1b0ba4f909efcd176c5f4caa4a00
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-13 20:24:26 +0100

    Commit 11003

---
+++ src/main/java/com/raphael/springbootpostgresqlconnection/constant/Gender.java
@@ -0,0 +1,36 @@
+package com.raphael.springbootpostgresqlconnection.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Gender {
+
+    MALE,
+    FEMALE,
+    NON_BINARY,
+    GENDERQUEER,
+    GENDERFLUID,
+    AGENDER,
+    BIGENDER,
+    TWO_SPIRIT,
+    ANDROGYNOUS,
+    DEMIGENDER,
+    NEUTROIS,
+    GENDER_QUESTIONING,
+    GENDER_NONCONFORMING,
+    TRANSGENDER,
+    TRANSMASCULINE,
+    TRANSFEMININE,
+    CISGENDER,
+    INTERSEX,
+    PANGENDER,
+    GENDER_VARIANT,
+    GENDER_EXPANSIVE,
+    GENDER_CREATIVE,
+    THIRD_GENDER,
+    OTHER,
+    PREFER_NOT_TO_SAY
+
+}
